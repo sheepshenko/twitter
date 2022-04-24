e785870402ad666676d88e3456d1d426bbc4e70b
Sheepshenko <sheepshenko@gmail.com>
2022-04-24 17:22:54 -0500

    Requerimiento 2: prueba de lista.

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -5,6 +5,10 @@ class UserService{
     static create(id, username, name){
         return new User(id, username, name, "Sin bio")
     }
+
+    static getInfo(user){
+        return Object.values(user)
+    }
 }
 
 
--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -8,4 +8,14 @@ describe("Test for UserService", () => {
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     })
+
+    test("2- Get all user data in a list", () => {
+        const user = UserService.create(1, "sheepshenko", "FernandoM")
+        const userInfoInList = UserService.getInfo(user)
+
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("sheepshenko")
+        expect(userInfoInList[2]).toBe("FernandoM")
+        expect(userInfoInList[3]).toBe("Sin bio")
+    })
 })
