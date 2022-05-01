393a97b0bea751864bb3102f3c0f2c189dba8ca8
Sheepshenko <sheepshenko@gmail.com>
2022-05-01 10:51:33 -0500

    Requerimiento 2: Views - Validar que se obtengan los valores correspondientes.

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -7,4 +7,11 @@ describe("Test for UserVies", () => {
         //https://jestjs.io/docs/using-matches#strings
         expect(resutl.error).toMatch(/payload no exist/)
     })
+
+    test("Return an eeror object when try to create a new user with a payload with invalid properties", () => {
+        const payload = {username: null, name: 12, id: "id"}
+        //const payload = {username: "null", name: "fer", id: 12}  //con valores que si pasan.
+        const result = UserView.createUser(payload);
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+    })
 })
