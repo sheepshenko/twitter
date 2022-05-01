6261f93879e7f97bd5f553c737ef9ad099df49a5
Sheepshenko <sheepshenko@gmail.com>
2022-05-01 10:58:55 -0500

    Creación del objeto con los 3 valores y validaciones.

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -21,4 +21,12 @@ describe("Test for UserVies", () => {
         const result = UserView.createUser(payload);
         expect(result.error).toMatch(/necesitan tener un valor válido/);
     })
+
+    test("Create a user by given valid payload", () => {
+        const payload = {username: "username", id: 1, name: "name"}
+        const result = UserView.createUser(payload);
+        expect(result.name).toBe("name");
+        expect(result.username).toBe("username");
+        expect(result.id).toBe(1);
+    })
 })
